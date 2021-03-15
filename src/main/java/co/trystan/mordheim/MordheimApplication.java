73b7b2cec3fcf783011bddab50b8ef5f433b95c8
Trystan VALENTIN <trystan.valentin@gmail.com>
2021-03-15 20:10:41 +0100

    init springboot

---
+++ src/main/java/co/trystan/mordheim/MordheimApplication.java
@@ -0,0 +1,13 @@
+package co.trystan.mordheim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MordheimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MordheimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/trystan/mordheim/MordheimApplicationTests.java
@@ -0,0 +1,13 @@
+package co.trystan.mordheim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MordheimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
